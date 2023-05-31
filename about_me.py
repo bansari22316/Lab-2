2832f905eaa400b64f226585ffd2b6ae0c10c63b
bansari22316 <133824230+bansari22316@users.noreply.github.com>
2023-05-31 14:12:20 -0400

    about me

--- about_me.py
+++ about_me.py
@@ -9,15 +9,26 @@ def main():
     # Step 2: Create a complex data structure that holds information about me
     about_me = {
         # TODO: Put full name into data structure
+        'name' : 'Bansari Patel',
         # TODO: Put student ID into data structure
+        'Student ID' : '10296950',
         # TODO: Put list of 3 pizza toppings into data structure
+        'Pizza Toppings' : [
+            'Tomatos',
+            'Green Pepper',
+            'Mushrooms'
+        ],
         'movies': [
             # TODO: Change this to a movie you like
             {
-                'title': 'The empire strikes back',
-                'genre': 'sci-fi'
+                'title': 'Twilight',
+                'genre': 'Rommantic'
             },
             # TODO: Add one more movie
+            {
+                'title' : 'Suryavanshi',
+                'genre' : 'Action'
+            },
         ]
     }
 
@@ -29,12 +40,12 @@ def main():
 
     # Step 5: Add pizza toppings to the data structure
     # TODO: Change to pizza toppings you like
-    add_pizza_toppings(about_me, ['soylent green', 'racht'])
+    add_pizza_toppings(about_me, ['Pineapple', 'Jalapenos'])
     print_pizza_toppings(about_me)
 
     # Step 6: Add another movie to the data structure
     # TODO: Change to a movie you like
-    add_movie(about_me, 'the lord of the rings', 'fantasy')
+    add_movie(about_me, 'The Fantasy Island', 'Suspense')
 
     # Step 7: Print a comma-separated list of movie genres
     print_movie_genres(about_me)
@@ -51,7 +62,8 @@ def print_student_name_and_id(my_info):
     # TODO: Complete function body per Step 3
     # Print sentence containing name
     # Print sentence containing student ID
-    print()
+    print(f"My name is {my_info['name']} and my student id is {my_info['Student ID']}.")
+    
 
 def print_pizza_toppings(my_info):
     """Prints a bullet list of favourite pizza toppings
@@ -62,7 +74,10 @@ def print_pizza_toppings(my_info):
     # TODO: Complete function body per Step 4
     # Print header "My favourite pizza toppings are:"
     # Print bullet list of favourite pizza toppings
-    print()
+    print(f"My favourite pizza toppings are: ")
+
+    for toppings_on_pizza in my_info['Pizza Toppings']:
+        print(f"- {toppings_on_pizza}")
 
 def add_pizza_toppings(my_info, toppings):
     """Adds some pizza toppings to the list of favourites
@@ -73,9 +88,14 @@ def add_pizza_toppings(my_info, toppings):
     """
     # TODO: Complete function body per Step 5
     # Append new pizza toppings to end of list 
+    my_info['Pizza Toppings'].extend(toppings)
     # Convert all pizza toppings to lowercase
     # Sort toppings list alphabetically
-    return
+    for i, toppings_on_pizza in enumerate(my_info['Pizza Toppings']):
+        my_info['Pizza Toppings'][i] = toppings_on_pizza.capitalize()
+
+    my_info['Pizza Toppings'].sort()
+    return add_pizza_toppings
 
 def add_movie(my_info, title, genre):
     """Adds a movie to the list of favourites
@@ -87,7 +107,12 @@ def add_movie(my_info, title, genre):
     """
     # TODO: Complete function body per Step 6
     # Create dictionary for new movie and add to movie list
-    return
+    another_movie = {
+        'Movie Title' : title,
+        'Movie genre' : genre
+    }
+    my_info['movies'].append(another_movie)
+    return add_movie
 
 def print_movie_genres(my_info):
     """Prints a sentence listing all favourite movie genres
@@ -96,7 +121,9 @@ def print_movie_genres(my_info):
         my_info (dict): Data structure containing information about me
     """
     # TODO: Complete function body per Step 7
-    print()
+    movie_names = [movie['genre'] for movie in my_info['movies']]
+    print(', '.join(movie_names), end='.')
+    
 
 def print_movie_titles(movie_list):
     """Prints a sentence listing all favourite movie titles
@@ -105,7 +132,10 @@ def print_movie_titles(movie_list):
         movie_list (list): List of favourite movies
     """
     # TODO: Complete function body per Step 8
-    print()
+    movie_list = [movie['title'] for movie in movie_list['movies']]
+    print(', '.join(movie_list), end='.')
+
+
 
 if __name__ == '__main__':
     main()
